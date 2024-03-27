53656c20cd601f73a12635524b92d06833230538
jchankim95 <kimjchan95@gmail.com>
2024-03-27 23:24:22 +0900

    feat : 임시방편으로 play클릭시 동영상 재생될 수 있도록 수정

--- react-disney-plus-app/src/components/Banner.js
+++ react-disney-plus-app/src/components/Banner.js
@@ -3,10 +3,13 @@ import instance from "../api/axios";
 import request from "../api/request";
 
 import './Banner.css'
+import styled from "styled-components";
 
 
 const Banner = (props) => {
   const [movie, setMovie] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
+
   useEffect(()=>{
     fetchData();
   },[])
@@ -31,34 +34,98 @@ const Banner = (props) => {
     return str?.length > n ? str.substring(0, n) + '...' : str;
   }
 
-  return (
-    <header
-      className="banner"
-      style={{
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-        backgroundSize: "cover",
-        backgroundPosition: "top center",
-        backgroundRepeat: "no-repeat",
-      }}
-    >
-      <div className="banner__contents">
-        <h1 className="banner__title">
-           {movie.title || movie.name || movie.orginal_name}
-        </h1>
-        <div className="banner__buttons">
-          {movie?.vidoes?.results[0]?.key &&
-            <button className="banner_button play">Play</button>
-          }
+  if(isClicked){
+    return(
+      <Container>
+        <HomeCaonter>
+          <Iframe 
+            width="640"
+            height="360" 
+            frameborder="0"
+            src="https://www.youtube.com/embed/3NZb75IvrLo?controls=0&autoplay=1&loop=1&mute=1"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerpolicy="strict-origin-when-cross-origin" 
+            allowfullscreen
+            >
+          </Iframe>
+          
+          {/* <Iframe
+            src={`https://www.youtube.com/embed/${movie.video.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+            width="640"
+            height="360"
+            frameborder="0"
+            allow="autoplay; fullscreen"
+          /> */}
+          <button onClick={()=>{setIsClicked(false)}}>X</button>
+        </HomeCaonter>
+      </Container>
+    )
+  }else{
+    return (
+      <header
+        className="banner"
+        style={{
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+          backgroundSize: "cover",
+          backgroundPosition: "top center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="banner__contents">
+          <h1 className="banner__title">
+             {movie.title || movie.name || movie.orginal_name}
+          </h1>
+          <div className="banner__buttons">
+            {movie?.vidoes?.results[0]?.key || true ?
+              <button className="banner_button play" onClick={()=>{
+                setIsClicked(true);
+              }}>Play</button>
+              :<></>
+            }
+          </div>
+          <p className="banner__description">
+            {truncate(movie.overview, 100)}
+          </p>
         </div>
-        <p className="banner__description">
-          {truncate(movie.overview, 100)}
-        </p>
-      </div>
-      <div className="banner--fadeBottom"/>
-
-
-    </header>
-  )
+        <div className="banner--fadeBottom"/>
+  
+  
+      </header>
+    )
+  }
 };
 
 export default Banner;
+
+
+const Container = styled.div`
+  display:  flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width : 100%;
+  height: 100vh;
+`;
+
+const HomeCaonter = styled.div`
+ width: 100%;
+ height: 100%;
+`;
+
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opcaity: 0.65;
+  border: none;
+
+  &::after{
+    content: "",
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+`
