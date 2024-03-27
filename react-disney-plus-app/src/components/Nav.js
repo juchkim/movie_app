52322f90c3237d6a64fdbac18b1cb0a9ef24f92b
jchankim95 <kimjchan95@gmail.com>
2024-03-27 21:35:58 +0900

    add : images 파일 추가 및 Nav 컴포넌트 작성

--- react-disney-plus-app/src/components/Nav.js
+++ react-disney-plus-app/src/components/Nav.js
@@ -1,10 +1,48 @@
 import React from "react"
+import styled from "styled-components";
+
+// 이미 스타이릴이 된 element를 만들수 있음. 단점으로는 css가 자동완성이 안됨
+const NavWrapper = styled.nav`
+  position: fiexd;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  display: inline-block;
+  cursor: pointer;
+  img{
+    display: block;
+    width: 100%;
+  }
+
+`
+
 
 const Nav = (props) => {
   return (
-    <div>
-      
-    </div>
+    <NavWrapper>
+      <Logo>
+        <img 
+          src="/images/logo.svg" 
+          alt="logo" 
+          onClick={()=>{window.location.href="/"}}  
+        />
+      </Logo>
+    </NavWrapper>
   )
 };
 
