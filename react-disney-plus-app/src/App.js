03121ea50e213a32b7017ac2b35782cd2410a122
jchankim95 <kimjchan95@gmail.com>
2024-03-27 21:58:13 +0900

    add : main화면에 Container 스타일 생성

--- react-disney-plus-app/src/App.js
+++ react-disney-plus-app/src/App.js
@@ -1,10 +1,29 @@
+import styled from 'styled-components';
 import Nav from './components/Nav';
+
+const Container = styled.div`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &::after{
+    background: url("/images/home-background.png") center center / cover no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opcaity : 1;
+    z-index: -1;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <Nav/>
-    </div>
+    </Container>
   );
 }
 
