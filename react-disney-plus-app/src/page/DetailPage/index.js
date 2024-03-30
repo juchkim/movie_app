a7260ea67349429e558fa577d899eb9962e916cb
jchankim95 <kimjchan95@gmail.com>
2024-03-30 23:52:02 +0900

    feat : DetailPage 수정

--- react-disney-plus-app/src/page/DetailPage/index.js
+++ react-disney-plus-app/src/page/DetailPage/index.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import instance from '../../api/axios';
 
 const DetailPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  let {movieId} = useParams();
+  // 라우터에서 설정해둔 파라매타를 가져오는 함수 useParams()
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchData(){
+      const res = await instance.get(`movie/${movieId}`);
+      setMovie(res.data);
+      console.log(res)
+    }
+    fetchData();
+  }, [movieId]);
+  if(!movie){ return null; }
+  else{
+    return (
+      <section>
+        <img 
+          className='modal__poster-img' 
+          src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+          alt={`${movie.backdrop_path}`}
+        />
+      </section>
+    )
+  }
 }
 
 export default DetailPage
