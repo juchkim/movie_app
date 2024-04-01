f9e0e7e936a685b47976e3409e838b05dbfdbb31
jchankim95 <kimjchan95@gmail.com>
2024-04-01 17:18:18 +0900

    add : swiper 기능 추가

--- react-disney-plus-app/src/components/Row.js
+++ react-disney-plus-app/src/components/Row.js
@@ -3,6 +3,17 @@ import instance from "../api/axios";
 import './Row.css';
 import MovieModal from "./MovieModal";
 
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import styled from "styled-components";
+
 const Row = ({title, id, fetchUrl}) => {
   const [movies, setMovies] = useState([]);
   const [selectMovie, setSelectMovie] = useState({});
@@ -23,36 +34,48 @@ const Row = ({title, id, fetchUrl}) => {
   },[fetchMovieData])
 
   return (
-    <div>
+    <Container>
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider__arrow-left">
-          <span className="arrow" onClick={()=>{
-            document.getElementById(id).scrollLeft += window.innerWidth/2 - 80;
-            //가로 스크롤 발생 시킨다음에 스크롤을 안보이게 하고 해당 부모 태그의 가로스크롤을 움직이게 하는 것(80은 세로스크롤바의 크기)
-          }}>
-            {"<"}
-          </span>
-        </div>
-        <div id={id} className="row__posters">
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true} //loop 기능을 사용할 것인지 
+        navigation // arrow 버튼 사용 유무
+        pagination={{clickable: true}} //페이지 버튼 보이게 할 것인지 확인
+        breakpoints={{
+          1378: {
+            slidesPerView: 6, //한번에 보이는 슬라이드 개수 
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5, //한번에 보이는 슬라이드 개수 
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4, //한번에 보이는 슬라이드 개수 
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3, //한번에 보이는 슬라이드 개수 
+            slidesPerGroup: 3,
+          },
+        }}
+      >
+        <Context id={id}>
           {movies.map(movie => (
-            <img 
-              className="row__poster" 
-              key={movie.id}
-              onClick={()=>handleClick(movie)} 
-              src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} 
-              alt={movie.title} 
-            />
+            <SwiperSlide>
+              <Wrap>
+                <img 
+                  key={movie.id}
+                  onClick={()=>handleClick(movie)} 
+                  src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} 
+                  alt={movie.title} 
+                />
+              </Wrap>
+            </SwiperSlide>
           ))}
-        </div>
-        <div className="slider__arrow-right">
-          <span className="arrow" onClick={()=>{
-            document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-          }}>
-            {">"}
-          </span>
-        </div>
-      </div>
+        </Context>
+      </Swiper>
       {modalOpen && 
       <MovieModal
         setModalOpen={setModalOpen}
@@ -60,8 +83,47 @@ const Row = ({title, id, fetchUrl}) => {
       />
       }
       
-    </div>
+    </Container>
   )
 };
 
 export default Row;
+
+const Container = styled.div`
+    padding: 0 0 26px;
+`
+
+const Context = styled.div`
+
+`
+const Wrap = styled.div`
+  width: 95%;
+  height: 95%;
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0/69%) 0px 26px 30px -10px,
+              rgb(0 0 0/73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+
+  img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    width: 100%;
+    transition: opacity 500ms ease-in-out;
+    z-index:1;
+  }
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+       rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(0.98);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`
